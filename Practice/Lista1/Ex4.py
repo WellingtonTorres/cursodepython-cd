a7f24e092f304fa4dbb975cfeaf33fe787e7a2b3
WellingtonTorres <wellingtontorresmelo@gmail.com>
2026-02-25 13:20:27 -0300

    resolucao completa do Ex4

---
+++ Practice/Lista1/Ex4.py
@@ -0,0 +1,21 @@
+# 4. Peça ao usuário para digitar um texto qualquer e: 
+# a)  Separe todas as palavras do texto em uma lista (considere que as palavras são separadas 
+# por espaço). 
+# b)  Converta essa lista em um set, para obter apenas as palavras únicas (eliminando 
+# repetições). 
+# c) Exiba: 
+# • A quantidade de palavras diferentes no texto. 
+# • A lista das palavras únicas em ordem alfabética.
+
+palavras = input("Digite as palavras: ").split()
+#Ao usar set ele elimina duplicado,
+# porém não mantém a ordem
+# palavrasUnicas = list(set(palavras))
+
+palavrasUnicas = list(dict.fromkeys(palavras))
+palavrasOdenadas = sorted(palavrasUnicas)
+
+print(f'Entrada: {palavras}')
+print(f'Palavras únicas: {palavrasUnicas}')
+print(f'Quantidade: {len(palavrasUnicas)}')
+print(f'Em ordem: {palavrasOdenadas}')
